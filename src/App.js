881fe0f8d9851096a69d536409fff17814a09ffa
“Pia <pia.hoelzl@gmx.at>
2024-01-16 18:39:19 +0100

    added styled-comp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,43 @@
-//imports
 import randomColor from 'randomcolor';
 import { useState } from 'react';
+import styled from 'styled-components';
+
+export default function App() {
+  const [textColor, setTextColor] = useState('');
+
+  const generatedColor = String(randomColor());
+
+  const divStyle = {
+    backgroundColor: generatedColor,
+    fontSize: '25px',
+    fontFamily: 'sans-serif',
+    padding: '20px',
+    paddingTop: '60px',
+    paddingBottom: '60px',
+  };
+
+  const Button = styled.button`
+    background-color: magenta;
+    color: black;
+    font-size: 35px;
+    padding: 60px 100px;
+    cursor: pointer;
+    border-radius: 20px;
+    margin: 20px 0px;
+  `;
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          setTextColor(generatedColor);
+        }}
+      >
+        generate
+      </Button>
+      <br />
+      <br />
+      <div style={divStyle}>random color {generatedColor}</div>
+    </>
+  );
+}
